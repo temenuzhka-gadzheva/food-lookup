0314170f864683fb6e5edaffd7712bc59ca77e84
temenuzhkagadzheva <temenuzhka.gadzheva.official@gmail.com>
2024-03-08 09:12:43 +0200

    Fix gitignore file to upload public folder

---
+++ food-lookup-app/src/components/Search.js
@@ -0,0 +1,29 @@
+// Search.js
+import React from 'react';
+import './Search.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+
+function Search({ onSearch }) {
+  const navigate = useNavigate();
+
+  return (
+    <> <div className="search-bar">
+      <FontAwesomeIcon icon={faSearch} className="search-icon" />
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search food by name"
+        onChange={(e) => onSearch(e.target.value)}
+      />
+    </div>
+      <div className="add-button-section">
+        <button onClick={() => navigate('/add-food')}>Add</button>
+      </div>
+    </>
+  );
+}
+
+export default Search;
---
+++ food-lookup-app/src/components/SelectedFoodsTable.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './SelectedFoodsTable.css'; // Make sure to create this CSS file
+
+function SelectedFoodsTable({ selectedFoods }) {
+  // Function to calculate total nutrients
+  const calculateTotalNutrients = () => {
+    return selectedFoods.reduce(
+      (totals, food) => {
+        totals.kcal += food.calories * food.count;
+        totals.protein += food.protein * food.count;
+        totals.fat += food.fats * food.count;
+        totals.carbs += food.carbs * food.count;
+        return totals;
+      },
+      { kcal: 0, protein: 0, fat: 0, carbs: 0 }
+    );
+  };
+
+  const totals = calculateTotalNutrients();
+
+  return (
+    <div className="food-table-container">
+      <h3>Selected foods</h3>
+      <table className="food-table">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Kcal</th>
+            <th>Protein (g)</th>
+            <th>Fat (g)</th>
+            <th>Carbs (g)</th>
+            <th>Selected count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {selectedFoods.map(food => (
+            <tr key={food.id}>
+              <td>{food.name}</td>
+              <td>{(food.calories * food.count).toFixed(2)}</td>
+              <td>{(food.protein * food.count).toFixed(2)}</td>
+              <td>{(food.fats * food.count).toFixed(2)}</td>
+              <td>{(food.carbs * food.count).toFixed(2)}</td>
+              <td>{food.count}</td>
+            </tr>
+          ))}
+          <tr className="totals-row">
+            <td>Total</td>
+            <td>{totals.kcal.toFixed(2)}</td>
+            <td>{totals.protein.toFixed(2)}</td>
+            <td>{totals.fat.toFixed(2)}</td>
+            <td>{totals.carbs.toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SelectedFoodsTable;
